5bacfdcf3931e2e99cb51dd887c9ad580737b30f
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-22 09:47:34 +0300

    Algo_MaxProfit Update

--- src/algo_question/MaxProfit.java
+++ src/algo_question/MaxProfit.java
@@ -9,7 +9,7 @@ package algo_question;
         Example 2:
         Input: prices = [7,6,4,3,1] Output: 0
         Explanation: In this case, no transactions are done and the max profit = 0.*/
-
+// Solution 1
 public class MaxProfit {
 
     public static int maxProfit(int[] prices) {
@@ -38,12 +38,43 @@ public class MaxProfit {
         return maxProfit;
     }
 
+    // Solution 2
+    // Time Complexity: O (n)
+    // Space Complexity: O (1)
+    static int getTheMaxProfit(int[] prices) {
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            minBuy = Math.min(minBuy, prices[i]);
+            int tempProfit = prices[i] - minBuy;
+            maxProfit = Math.max(maxProfit, tempProfit);
+        }
+        return maxProfit;
+    }
+
+
+// Solution 3
+    // Time Complexity: O (n)
+    // Space Complexity: O (1)
+    public int getMaxProfitagain(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profitSoFar =0;
+        int currentProfit;
+        for (int price : prices) {
+            if (price < min) min = price;
+            currentProfit = price - min;
+            if (currentProfit > profitSoFar)
+                profitSoFar = currentProfit;
+        }
+        return profitSoFar;
+    }
+
     public static void main(String[] args) {
         // Test cases
         int[] prices1 = {7, 1, 5, 3, 6, 4};
-        int[] prices2 = {7, 6, 4, 3, 1};
+        int[] prices2 = {1, 6, 4, 3, 1,8};
 
         System.out.println(maxProfit(prices1)); // Output: 5
-        System.out.println(maxProfit(prices2)); // Output: 0
+        System.out.println(getTheMaxProfit(prices2)); // Output: 0
     }
 }
