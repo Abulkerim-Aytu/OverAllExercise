95fc0f8184482a3b9fd7e73b92be6d8cd88fe3e0
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-23 11:35:38 +0300

    Algo_str

---
+++ src/algo_question/Kata.java
@@ -0,0 +1,11 @@
+package algo_question;
+
+class Kata {
+
+  //We need a function that can transform a number (integer) into a string.
+  public static String numberToString(int num) {
+    // Return a string of the number here!
+    String st = String.valueOf(num);
+    return st;
+  }
+}
